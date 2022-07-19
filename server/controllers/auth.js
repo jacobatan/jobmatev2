25d5b103012bc6938c6dd9aa30453a527d76e79e
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-07-19 16:12:45 +1200

    feat: password hashing middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,12 @@
-const User = require("../")
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  res.send("register user");
+  // password is first hashed in schema
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = async (req, res) => {
