edfbb50d8aba06e7a8198689b2b630dd6723dbf0
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-07-31 10:36:34 +1200

    feat: auth and job api

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const User = require("./models/User");
 // DB connections
 const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
 
 // routers
 const authRouter = require("./routes/auth");
@@ -20,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,8 +7,7 @@ const register = async (req, res) => {
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
-};
-
+}
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
