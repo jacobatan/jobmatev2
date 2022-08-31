d84543a9368c71cc133873512a50d31749e726ab
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-08-31 16:00:45 +1200

    experimental folder structure changes

--- server/middleware/not-found.js
+++
@@ -1,6 +0,0 @@
-const notFound = (req, res) => {
-  console.log("nor found middle ware");
-  return res.status(404).send("Route does not exist");
-};
-
-module.exports = notFound;
--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const { login, register } = require("../controllers/auth");
-
-const express = require("express");
-const router = express.Router();
-
-router.route("/register").post(register);
-router.route("/login").post(login);
-
-module.exports = router;
