d901805a0fb8fdf3568e8dc80dcc23a00924b97a
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-08-31 14:59:05 +1200

    feat: security

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 require("express-async-errors");
 
 const User = require("./models/User");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // DB connections
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
@@ -17,7 +22,12 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
