732f6e20035bbf8919ce4885944187e7ae3b64a3
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-08-31 16:37:42 +1200

    bug: cors error

--- app.js
+++ app.js
@@ -23,10 +23,15 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.set("trust proxy", 1);
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(xss());
 // extra packages
 
