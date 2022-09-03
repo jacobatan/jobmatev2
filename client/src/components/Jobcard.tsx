f2ed3c9d9ee5b2c7a9cb5fc6b997f7534d420008
jacobatan <jacob.tan229@gmail.com>
2022-09-04 01:45:34 +1200

    feat: delete func and modal

--- client/src/components/Jobcard.tsx
+++ client/src/components/Jobcard.tsx
@@ -4,14 +4,36 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Cookies from 'js-cookie';
 
 interface iJobcard {
+  id: string;
   company: string;
   position: string;
   status: string;
+  deleteJob: (id: string) => Promise<void>;
 }
 
-export default function Jobcard({ company, position, status }: iJobcard) {
+export default function Jobcard({ deleteJob, id, company, position, status }: iJobcard) {
+  const auth = Cookies.get();
+  const bearer = 'Bearer ' + auth.token;
+
+  const DeleteJob = async () => {
+    try {
+      const link = `${process.env.REACT_APP_LOCAL_LINK}/jobs/${id}`
+      const resp = await fetch(link, {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+          'Authorization': bearer,
+          'Content-Type': 'application/json'
+        },
+      })
+      const data = await resp.json();
+    } catch (e) {
+      console.log(e)
+    }
+  }
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
@@ -26,7 +48,8 @@ export default function Jobcard({ company, position, status }: iJobcard) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">edit</Button>
+        <Button size="small" onClick={() => console.log(id)}>edit</Button>
+        <Button size="small" onClick={() => deleteJob(id)}>delete</Button>
       </CardActions>
     </Card>
   );
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Grid, Paper, Box, Typography, TextField, Button, MenuItem } from '@mui/material'
-import { Link, Navigate } from "react-router-dom"
+import { Grid, Paper, Box, Typography, TextField, Button, MenuItem, Modal } from '@mui/material'
 import Cookies from 'js-cookie'
 import Jobcard from '../components/Jobcard';
 
@@ -16,33 +15,64 @@ const statuses = [
   },
 ];
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const Dashboard = () => {
   const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
 
   const [jobDetails, setJobDetails] = useState({ company: "", position: "", status: "pending" })
   const [jobs, setJobs] = useState<any[]>([])
+  const [open, setOpen] = useState(false);
 
   const auth = Cookies.get();
   const bearer = 'Bearer ' + auth.token;
 
-  useEffect(() => {
-    const GetAllJobs = async () => {
-      try {
-        const link = `${process.env.REACT_APP_LOCAL_LINK}/jobs`
-        const resp = await fetch(link, {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            'Authorization': bearer,
-            'Content-Type': 'application/json'
-          },
-        })
-        const data = await resp.json();
-        setJobs(data.jobs)
-      } catch (e) {
-        console.log(e)
-      }
+  const GetAllJobs = async () => {
+    try {
+      const link = `${process.env.REACT_APP_LOCAL_LINK}/jobs`
+      const resp = await fetch(link, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Authorization': bearer,
+          'Content-Type': 'application/json'
+        },
+      })
+      const data = await resp.json();
+      setJobs(data.jobs)
+    } catch (e) {
+      console.log(e)
     }
+  }
+
+  const DeleteJob = async (id: string) => {
+    try {
+      const link = `${process.env.REACT_APP_LOCAL_LINK}/jobs/${id}`
+      const resp = await fetch(link, {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+          'Authorization': bearer,
+          'Content-Type': 'application/json'
+        },
+      })
+      GetAllJobs()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
     GetAllJobs()
   }, [])
 
@@ -59,42 +89,51 @@ const Dashboard = () => {
         body: JSON.stringify(jobDetails),
       })
       const data = await resp.json();
+      setOpen(false);
+      GetAllJobs()
     } catch (err) {
       console.log(err)
     }
   }
 
+
   return (
     <>
-      <Grid>
-        <Paper elevation={20} style={paperStyle}>
-          <Grid alignItems="center">
-            <Typography variant="h3">create a job</Typography>
-            <Typography variant='caption' gutterBottom>creat a job</Typography>
-          </Grid>
-          <Box component="form">
-            <TextField fullWidth label='Company' type="text" placeholder="Enter your company" sx={{ my: 2 }} onChange={(e) => setJobDetails({ ...jobDetails, company: e.target.value })} />
-            <TextField fullWidth label='Position' type="text" placeholder="Enter your position" sx={{ my: 2 }} onChange={(e) => setJobDetails({ ...jobDetails, position: e.target.value })} />
-            <TextField
-              fullWidth
-              id="outlined-select-currency"
-              select
-              label="Select"
-              onChange={(e) => setJobDetails({ ...jobDetails, status: e.target.value })}
-              helperText="Please select the status of your job"
-            >
-              {statuses.map((status) => (
-                <MenuItem key={status.value} value={status.value}>
-                  {status.value}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Button variant='contained' color='primary' onClick={onSubmit}>Create!</Button>
-          </Box>
-        </Paper >
-      </Grid >
+      <Button onClick={() => setOpen(true)}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Grid>
+          <Paper elevation={20} style={paperStyle}>
+            <Grid alignItems="center">
+              <Typography variant="h3">create a job {auth.name}</Typography>
+              <Typography variant='caption' gutterBottom>create a job</Typography>
+            </Grid>
+            <Box component="form">
+              <TextField fullWidth label='Company' type="text" placeholder="Enter your company" sx={{ my: 2 }} onChange={(e) => setJobDetails({ ...jobDetails, company: e.target.value })} />
+              <TextField fullWidth label='Position' type="text" placeholder="Enter your position" sx={{ my: 2 }} onChange={(e) => setJobDetails({ ...jobDetails, position: e.target.value })} />
+              <TextField
+                fullWidth
+                id="outlined-select-currency"
+                select
+                label="Select"
+                onChange={(e) => setJobDetails({ ...jobDetails, status: e.target.value })}
+                helperText="Please select the status of your job"
+              >
+                {statuses.map((status) => (
+                  <MenuItem key={status.value} value={status.value}>
+                    {status.value}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Button variant='contained' color='primary' onClick={onSubmit}>Create!</Button>
+            </Box>
+          </Paper >
+        </Grid >
+      </Modal>
       {
-        jobs?.map((job) => <Jobcard key={job._id} company={job.company} position={job.position} status={job.status} />)
+        jobs?.map((job) => <Jobcard key={job._id} id={job._id} deleteJob={DeleteJob} company={job.company} position={job.position} status={job.status} />)
       }
     </>
   )
