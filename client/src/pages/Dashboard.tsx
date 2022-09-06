b5db6e93a998114670e58a95ad4ed6d6caf67fdc
jacobatan <jacob.tan229@gmail.com>
2022-09-06 12:05:37 +1200

    refactor: moved some of the existing modal code to the navbar

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
-import { Button, } from '@mui/material'
+// import { Button, } from '@mui/material'
 import Jobcard from '../components/Jobcard';
-import { GetAllJobs, CreateJob } from "../api/Jobapi"
+import { GetAllJobs } from "../api/Jobapi"
 import DBModal from '../components/DBModal';
 
 const Dashboard = () => {
 
-  const [jobDetails, setJobDetails] = useState({ company: "", position: "", status: "pending" })
   const [jobs, setJobs] = useState<any[]>([])
   const [open, setOpen] = useState(false);
 
@@ -14,14 +13,9 @@ const Dashboard = () => {
     GetAllJobs({ setMyVar: setJobs })
   }, [])
 
-  const onSubmit = async () => {
-    setOpen(false);
-    CreateJob({ setMyVar: setJobs, obj: jobDetails });
-  }
-
   return (
     <>
-      <Button onClick={() => setOpen(true)}>Open modal</Button>
+      {/* <Button onClick={() => setOpen(true)}>Open modal</Button> */}
       <DBModal myVar={open} setMyVar={setOpen} setJobs={setJobs} />
       <div className="grid grid-cols-1 gap-4 w-11/12 md:grid-cols-2 lg:w-5/6 lg:grid-cols-3 mx-auto">
         {
